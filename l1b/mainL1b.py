658c0ad3e9005cf857f84ab058bb5ecd7ecc0e54
juan05uc3m <100549357@alumnos.uc3m.es>
2025-09-18 16:06:22 +0200

    Update prueba
    Cambios en dos cosillas

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\Users\juantej\PycharmProjects\test_eodp\auxiliary'
-indir = r"C:\Users\juantej\PycharmProjects\EODP-TS-L1B\input"
-outdir = r"C:\\Users\juantej\PycharmProjects\EODP-TS-L1B\myoutputs"
+auxdir = r'C:\\Users\\juantej\\PycharmProjects\\test_eodp\\auxiliary'
+indir = r"C:\\Users\\juantej\\PycharmProjects\\EODP-TS-L1B\\input"
+outdir = r"C:\\Users\\juantej\\PycharmProjects\\EODP-TS-L1B\\myoutputs"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
