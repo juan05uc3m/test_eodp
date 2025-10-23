5a60ad017a3e255694d3233c4203f3b67d6c5957
juantej05 <juantejro05@gmail.com>
2025-10-23 16:07:34 +0200

    Dia 3 completo
    Apartado 1 y 2 enteros.

--- ism/test/testism.py
+++ ism/test/testism.py
@@ -1,7 +1,77 @@
 #En este archivo se realizará un test de los resultados obtenidos en el módulo ism
+from common.io.writeToa import writeToa, readToa
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Cargo los outputs suyos
+toa_ism_isrf_0 = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_isrf_VNIR-0.nc")
+toa_ism_isrf_1 = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_isrf_VNIR-1.nc")
+toa_ism_isrf_2  = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_isrf_VNIR-2.nc")
+toa_ism_isrf_3  = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_isrf_VNIR-3.nc")
+
+#cargo mis outputs
+
+toa_ism_isrf_0_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_isrf_VNIR-0.nc")
+toa_ism_isrf_1_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_isrf_VNIR-1.nc")
+toa_ism_isrf_2_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_isrf_VNIR-2.nc")
+toa_ism_isrf_3_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_isrf_VNIR-3.nc")
 
 
-toa_ism_isrf_0_juan = readToa(r"C:\\Users\juantej\PycharmProjects\EODP-TS-L1B\output", "l1b_toa_VNIR-0.nc")
-toa_ism_isrf_1_juan = readToa(r"C:\\Users\juantej\PycharmProjects\EODP-TS-L1B\output", "l1b_toa_VNIR-1.nc")
-toa_ism_isrf_2_juan  = readToa(r"C:\\Users\juantej\PycharmProjects\EODP-TS-L1B\output", "l1b_toa_VNIR-2.nc")
-toa_ism_isrf_3_juan  = readToa(r"C:\\Users\juantej\PycharmProjects\EODP-TS-L1B\output", "l1b_toa_VNIR-3.nc")
+#Ahora cargo los ism opticos de sus outputs y de mis ooutputs
+
+toa_ism_opt_0 = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_optical_VNIR-0.nc")
+toa_ism_opt_1 = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_optical_VNIR-1.nc")
+toa_ism_opt_2 = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_optical_VNIR-2.nc")
+toa_ism_opt_3 = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\output", "ism_toa_optical_VNIR-3.nc")
+
+#los mios
+
+toa_ism_opt_0_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_optical_VNIR-0.nc")
+toa_ism_opt_1_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_optical_VNIR-1.nc")
+toa_ism_opt_2_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_optical_VNIR-2.nc")
+toa_ism_opt_3_juan = readToa(r"C:\\Users\juant\EODP_DATA\EODP-TS-ISM\myoutputdia3", "ism_toa_optical_VNIR-3.nc")
+
+
+#Ahora hay que comprobar si tienen error
+#utilizo la misma funcion que en 1lb
+
+def check_band(toa_my, toa_ref, tol=0.01):
+    # Diferencia relativa en porcentaje
+    diff = np.abs(toa_my - toa_ref) / np.maximum(toa_ref, 1e-12) * 100
+
+    # Media y sigma
+    mu = np.mean(diff)
+    sigma = np.std(diff)
+
+    # Límite 3-sigma
+    threshold = mu + 3*sigma
+
+    # ¿Está por debajo del 0.01%?
+    ok = threshold < tol
+    return mu, sigma, threshold, ok
+
+for i, (my, ref) in enumerate([(toa_ism_isrf_0_juan, toa_ism_isrf_0), (toa_ism_isrf_1_juan, toa_ism_isrf_1),
+                               (toa_ism_isrf_2_juan, toa_ism_isrf_2), (toa_ism_isrf_3_juan, toa_ism_isrf_3)]):
+    mu, sigma, thr, ok = check_band(my, ref)
+    print(f"Banda {i}: media={mu:.3f}, sigma={sigma:.3f}, límite_3σ={thr:.3f}, Cumple={ok}")
+
+    if ok:
+        print(f"La banda {i} cumple con el umbral establecido.\n")
+    else:
+        print(f"La banda {i} **NO** cumple con el umbral establecido.\n")
+
+#Ahora lo compruebo para los resultados del ism_optical
+
+for i, (my, ref) in enumerate([(toa_ism_opt_0_juan, toa_ism_opt_0), (toa_ism_opt_1_juan, toa_ism_opt_1),
+                               (toa_ism_opt_2_juan, toa_ism_opt_2), (toa_ism_opt_3_juan, toa_ism_opt_3)]):
+    mu, sigma, thr, ok = check_band(my, ref)
+    print(f"Banda {i}: media={mu:.3f}, sigma={sigma:.3f}, límite_3σ={thr:.3f}, Cumple={ok}")
+
+    if ok:
+        print(f"La banda {i} opt cumple con el umbral establecido.\n")
+    else:
+        print(f"La banda {i} opt **NO** cumple con el umbral establecido.\n")
+
+
+
+
