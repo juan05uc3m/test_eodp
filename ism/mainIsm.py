046d4327dedc9c63505bfa64bb55a3ea90d91117
juantej05 <juantejro05@gmail.com>
2025-10-27 13:36:27 +0100

    Final code
    Se han hecho los cambios para poder responder el ultimo modulo. Este codigo ya se ejecuto hace 2 semanas en clase.

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,8 +5,9 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\juant\\EODP_DATA\\eodp_students-master\\auxiliary'
-indir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-ISM\\input\\gradient_alt100_act150"
-outdir = r"C:\\Users\\juant\\EODP_DATA\\\EODP-TS-ISM\\myoutputdia4"
+#indir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-ISM\\input\\gradient_alt100_act150"
+indir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-E2E\\sgm_out"
+outdir = r"C:\\Users\\juant\\EODP_DATA\\\EODP-TS-ISM\\myoutputism"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,9 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\juant\\EODP_DATA\\eodp_students-master\\auxiliary'
-indir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-L1B\\input"
-outdir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-L1B\\myoutputs_eq_copia"
+#indir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-L1B\\input"
+indir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-ISM\\myoutputismc"
+outdir = r"C:\\Users\\juant\\EODP_DATA\\EODP-TS-L1B\\myputputs_with_ism_imput"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
